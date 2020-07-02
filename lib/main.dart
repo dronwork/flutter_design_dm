67bf86eedfd06e70f730986a91777b1d20ad7f2c
Andrei Keida <dronwork@outlook.com>
2020-07-02 23:37:01 +0300

    'testing'

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,38 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:keepapp/blocs/HomeBloc.dart';
+import 'package:keepapp/blocs/LoginBloc.dart';
+import 'package:keepapp/screen/LoginPage.dart';
+import 'package:overlay_support/overlay_support.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
-
-
-class MyApp extends StatelessWidget {
-  void func()
-  {
-    print("Hello F");
-  }
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+main() {
+  if (defaultTargetPlatform != TargetPlatform.android ||
+      defaultTargetPlatform != TargetPlatform.iOS) {
+    /// For desktop apps only
+    debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
   }
 
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (context) => LoginBloc()),
+      ChangeNotifierProvider(create: (context) => HomeBloc()),
+    ],
+    //   create: (context) => LoginBloc(),
+    child: OverlaySupport(
+      child: MaterialApp(
+        home: LoginPage(),
+        theme: ThemeData(
+          fontFamily: 'OpenSans',
+          primaryColor: Colors.deepPurple,
+          accentColor: Color(0xffff470b),
+          primaryIconTheme: IconThemeData(color: Colors.deepPurple),
+          iconTheme: IconThemeData(color: Colors.white),
+          accentIconTheme: IconThemeData(color: Color(0xffff470b)),
         ),
+        debugShowCheckedModeBanner: false,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+    ),
+  ));
 }
